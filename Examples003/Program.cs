0de9b4aa34e1b4e5a659df1cc1a37aa52f81ced0
Dimasik <pak.dima.2006@mail.ru>
2023-08-01 12:22:34 +0600

    Random numbers

--- Examples003/Program.cs
+++ Examples003/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10); // 1.2.3.4.5....9
+Console writeLine(numberA);
+int numberB = new Random().Next(1,10);
+Console writeLine(numberB);
 int result =  numberA + numberB;
 Console.WriteLine(result);
