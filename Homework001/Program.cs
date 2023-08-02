f5402951896b54ff281bdefd4cea7471991a57c9
Dimasik <pak.dima.2006@mail.ru>
2023-08-02 13:34:04 +0600

    Domashka seminar 1

---
+++ Homework001/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдает,какое число больше,а какое меньше
+Console.WriteLine("Введите числа: ");
+int nA = Convert.ToInt32(Console.ReadLine());
+int nB = Convert.ToInt32(Console.ReadLine());
+if (nA > nB)
+{
+    Console.WriteLine($"Большее число {nA}, меньшее число {nB}");
+}
+else if (nA == nB)
+{
+    Console.WriteLine($"число {nB = nA}");
+}
+else if (nA < nB)
+{
+    Console.WriteLine($"большее число {nB}, меньшее число {nA}");
+}
